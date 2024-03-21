3a6fae5b4a95ccd878e5232061130c9c4cf6ea3f
Don Hansen <donhansen@Dons-Mac-mini.local>
2024-03-20 22:53:22 -0500

    loading static files from client, continuing forward with building twitch service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TwitchModule } from './twitch/twitch.module';
 
 @Module({
-  imports: [TwitchModule],
+  imports: [
+    ConfigModule.forRoot(), 
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client')
+    }),
+    TwitchModule],
   controllers: [AppController],
   providers: [AppService],
 })
